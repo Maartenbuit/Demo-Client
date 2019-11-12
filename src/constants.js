87d381585fd70064fef9bcdd95f1c8445dfe5176
Maartenbuit <maartenbuitelaar@hotmail.com>
2019-11-12 08:59:39 +0100

    small clean up

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
-//export const url = 'http://localhost:4000'
-export const url = 'https://frozen-citadel-01903.herokuapp.com'
+//local or heroku server
+
+export const url = 'http://localhost:4000'
+//export const url = 'https://frozen-citadel-01903.herokuapp.com'
